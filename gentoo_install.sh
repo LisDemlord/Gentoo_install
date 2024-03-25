c3843a721ce43ddf938d47d31b11f06218868358
Lis Demlord <lis.demlord@gmail.com>
2024-03-25 11:37:45 +0500

    partitioning disk

--- gentoo_install.sh
+++ gentoo_install.sh
@@ -4,7 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-fdisk /dev/vda
-p
-g
-p
+echo -ne "label:gpt\nsize=2GiB,type=\"EFI System\"\nsize=12GiB,type=\"Linux swap\"\nsize=+,type=\"Linux root (x86-64)\"\n" | sfdisk /dev/vda
