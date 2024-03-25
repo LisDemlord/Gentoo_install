7e35f337f4224fae986292372049404312fd2dc7
Lis Demlord <lis.demlord@gmail.com>
2024-03-25 13:53:45 +0500

    download stage3 files

--- gentoo_install.sh
+++ gentoo_install.sh
@@ -5,3 +5,20 @@ set -o nounset
 set -o pipefail
 
 echo -ne "label:gpt\nsize=2GiB,type=\"EFI System\"\nsize=12GiB,type=\"Linux swap\"\nsize=+,type=\"Linux root (x86-64)\"\n" | sfdisk /dev/vda
+
+mkfs.vfat -F 32 /dev/vda1
+mkfs.xfs /dev/vda3
+mkswap /dev/vda2
+swapon /dev/vda2
+
+mkdir --parents /mnt/gentoo
+mount /dev/vda3 /mnt/gentoo
+
+mkdir --parents /mnt/gentoo/efi
+mount /dev/vda1 /mnt/gentoo/efi  # Монтируем раздел EFI в /mnt/gentoo/efi
+
+cd /mnt/gentoo
+date
+
+wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20240317T170433Z/stage3-amd64-systemd-mergedusr-20240317T170433Z.tar.xz
+tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
